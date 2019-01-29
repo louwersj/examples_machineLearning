ff31f064460e8d57bd5840e4c66b3324a7ed42ef
Johan Louwers <louwersj@gmail.com>
2019-01-29 15:28:26 +0100

    repair style violations

--- supervised/classifier/example_1.py
+++ supervised/classifier/example_1.py
@@ -5,7 +5,6 @@ __copyright__ = "Copyright 2019, Johan Louwers"
 __license__ = "MIT"
 __email__ = "louwersj@gmail.com"
 
-
 ''' 
     This example code is based upon an example provided by Google and showcases classifier used in a supervised 
     machine learning example to predict if an object is an apple (0) or an orange (1) based upon the weight and the 
@@ -14,7 +13,6 @@ __email__ = "louwersj@gmail.com"
     When using a clf.predict and provide two new features if model will decide if it is an apple (0) or an orange (1) 
     based upon the learning data it already has in the form a features and label set.  
 '''
-
 
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
--- supervised/classifier/example_2.py
+++ supervised/classifier/example_2.py
@@ -10,12 +10,12 @@ print iris.target_names
 print iris.data[0]
 print iris.target[0]
 
-#for i in range(len(iris.target)):
+# for i in range(len(iris.target)):
 #    print "Example %d: label: %s, feature %s" % (i, iris.target[i], iris.data[i])
 
 
 # training data
-test_idx = [0,50,100]
+test_idx = [0, 50, 100]
 train_target = np.delete(iris.target, test_idx)
 train_data = np.delete(iris.data, test_idx, axis=0)
 
@@ -31,7 +31,7 @@ print test_target
 print clf.predict(test_data)
 
 
-#viz code
+# viz code
 
 from sklearn.externals.six import StringIO
 import pydot
@@ -39,12 +39,12 @@ import pydot
 dot_data = StringIO()
 tree.export_graphviz(clf,
                      out_file=dot_data,
-                     feature_names = iris.feature_names,
-                     class_names = iris.target_names,
+                     feature_names=iris.feature_names,
+                     class_names=iris.target_names,
                      filled=True, rounded=True,
                      impurity=False)
 
-#graph = pydot.graph_from_dot_data(dot_data.getvalue())
+# graph = pydot.graph_from_dot_data(dot_data.getvalue())
 
 (graph,) = pydot.graph_from_dot_data(dot_data.getvalue())
 
