01baf9e5fc2e5b041128f1aa52af4941030812c4
Johan Louwers <louwersj@gmail.com>
2019-02-06 14:35:55 +0100

    small changes

--- matplotlib/scatter/plt_scatter_example_1.py
+++ matplotlib/scatter/plt_scatter_example_1.py
@@ -3,8 +3,8 @@ import matplotlib
 matplotlib.use('Agg') #quick fix to select the backend and prevent MacOs issues
 import matplotlib.pyplot as ppl
 
-# create a dataframe using Pandas, reading the .csv file with a ';' delimiter
-df = pd.read_csv('../../data/05-gemiddelde-afstand-tussen-woonadres-leerling-en-schoolvestiging-2017-2018.csv', delimiter=";", thousands=',')
+# create a dataframe using Pandas, reading the .csv file with a ';' delimiter.
+df = pd.read_csv('../../data/dataset_2.csv', delimiter=";", thousands=',')
 
 # Uncomment if you want to see the head of the dataframe
 print(df.head())
