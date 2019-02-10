ecc4204c9e9c6931d1afb253525dfff394d2a0fd
Johan Louwers <louwersj@gmail.com>
2019-02-10 20:08:32 +0100

    added new example code, did some cleanup, added new comments, fixed a small bug.

--- matplotlib/simple_example/plt_simple_example_1.py
+++ matplotlib/simple_example/plt_simple_example_1.py
@@ -12,13 +12,25 @@ __email__ = "louwersj@gmail.com"
     situation and operating system. 
 
     This example shows the most simple implementation of matplotlib and pyplot to plot a line graph and save it to a 
-    local file. The data used is hardcoded. 
+    local file. The data used is hardcoded. For this we use the average daily temp of a river during a one month periode
+    by providing pyplot with two lists. The raw data can be located in dataset_1.csv
 '''
 
 import matplotlib
-matplotlib.use('agg') #quick workarround for issue with matplotlib unable to find backend on MacOS
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], [1,2,4,7,8,9,8,6,5,3,1,0,2,4,6,8,9,3,2,1])
+# provide two lists, one for the values containing the average temperature of the water during a 24 hour period and one
+# for the day of the month.
+averageTemp = [23, 20.5, 22, 30.5, 31, 27.5, 26.25, 26.5, 23, 23.5, 27, 26.5, 28.5, 24.25, 12.25, 11.25, 17.5, 18, 15.5, 19.5, 15, 21, 21, 20, 28, 26.5, 23.5, 19.25, 13.5, 19]
+dayOfMonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
+# use pyplot to plot both the lists on the graph.
+plt.plot(dayOfMonth, averageTemp)
+
+# save the plot as a figure using savefig.
 plt.savefig('plt_simple_example_1.png')
+
+
+
+
---
+++ matplotlib/simple_example/plt_simple_example_2.py
@@ -0,0 +1,42 @@
+__author__ = "Johan Louwers"
+__copyright__ = "Copyright 2019, Johan Louwers"
+__license__ = "MIT"
+__email__ = "louwersj@gmail.com"
+
+''' 
+    This example code is based a private example code and comes as-is without any warranty under the mentioned license.
+
+    DO NOTE : due to a 'bug' when using a virtualenv in combination with matplotlib on MacOS we have stated the used
+    backend explicitly using matplotlib.use('agg') which might not be required in every situation and might not work
+    in your specific situation. In case the code gives errors please check the backend setting required for your specific
+    situation and operating system. 
+
+    This example builds upon the code provided in plt_simple_example_1.py, the main difference is that we do not use
+    a list to provide the values, we use pandas to read the data from the file and provide it to pyplot. This makes the
+    code much more usable, as a side-effect we will now see all the data available the original dataset (dataset-1.csv)
+'''
+
+import pandas as pd
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+# Create a dataframe (df) by instructing pandas to read the dataset. This replaces the need to have two lists and makes
+# the code more usable in real-life.
+df = pd.read_csv('../../data/dataset_1.csv')
+
+# Populate ax wih plt.gca (gca stands for 'get current axis')
+ax = plt.gca()
+
+# use the dataframe to do the plot, in the backend it will use matplotlib however it is initiated by the dataframe in
+# this case.
+df.plot(kind='line',x='ds',y='y',ax=ax)
+
+
+# save the plot as a figure using savefig, we use plt instead of df (dataframe) as we now pull it to the frontend and we
+# are not using the backend part of matplotlib. This should provide you with an 'ugly' graph.
+plt.savefig('plt_simple_example_2.png')
+
+
+
+
