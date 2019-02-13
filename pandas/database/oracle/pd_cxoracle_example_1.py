1ad091d94b29a985a54c55285c0d1f0a03db3b54
Johan Louwers <louwersj@gmail.com>
2019-02-13 16:30:56 +0100

    small cleanup at cx_oracle example.

--- pandas/database/oracle/pd_cxoracle_example_1.py
+++ pandas/database/oracle/pd_cxoracle_example_1.py
@@ -6,16 +6,16 @@ import cx_Oracle
 
 
 
-orclUserName='pandas_test'
-orclPassword='pandas_test'
+orclUserName = 'pandas_test'
+orclPassword = 'pandas_test'
 orclHost = '192.168.33.10'
 orclPort = '1521'
-orclServiceName= 'pdb1'
+orclServiceName = 'pdb1'
 
-dsn = cx_Oracle.makedsn(host=orclHost,port=orclPort,sid=None,service_name=orclServiceName)
+dsn = cx_Oracle.makedsn(host=orclHost, port=orclPort, sid=None, service_name=orclServiceName)
 
 
-connection = cx_Oracle.connect(user=orclUserName, password=orclPassword, dsn= dsn ,encoding = "UTF-8", nencoding = "UTF-8")
+connection = cx_Oracle.connect(user=orclUserName, password=orclPassword, dsn=dsn, encoding="UTF-8", nencoding="UTF-8")
 
 
 query = """select empno from emp"""
